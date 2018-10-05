82826978b086836d49a49597101afaaf1d5f8f6f
Ashley Wong <ashwong04@gmail.com>
2018-10-05 00:54:46 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,8 @@ class Triangle
   def kind
     if @length <= 0 || @width <=0 || @height <= 0
       raise TriangleError
+    elsif @length+@width <= @height || @width+@height <= @length || @length+@height <= @width
+      raise TriangleError
     elsif @length == @width && @width == @height
       return :equilateral
     elsif @length == @width || @length == @height || @width == @height
